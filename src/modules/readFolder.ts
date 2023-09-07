612fa546b318d442010a8b1908c45bd52c645287
Avelyn Choi <avelynhc@gmail.com>
2023-09-07 19:36:31 -0400

    Code clean up

--- src/modules/readFolder.ts
+++ src/modules/readFolder.ts
@@ -1,33 +1,16 @@
-var fs = require("fs");
-var fileModule = require("./readFile");
-var html = require("./convertToHTML");
-var body = "";
+import readFolderFs from "fs";
+const fileModule = require("./readFile");
 
-module.exports.readFolder = function (path: any, cssLink: any, outputContainer: any) {
-    fs.readdir(path, function (err: any, files: any[]) {
-        if (err) {
-            return console.log(err);
-        }
+module.exports.readFolder = function (path: any, cssLink: any, outputFolder: any) {
+    readFolderFs.readdir(path, function (err: any, files: any[]) {
+        if (err) return console.log(err);
 
         files.forEach(function (file) {
-            var fileName = fileModule.readFile(
+            fileModule.readFile(
                 `${path}/${file}`,
                 cssLink,
-                outputContainer
+                outputFolder
             );
-
-            // index.html body
-            body += `<h5><a href={${path}/${encodeURI(
-                fileName
-            )}.html}>${fileName}</h5>\n`;
         });
-
-        // create index.html
-        html.convertToHTML(
-            "index",
-            cssLink,
-            `<h4>Generated Sites</h4>\n${body}`,
-            outputContainer
-        );
     });
 };

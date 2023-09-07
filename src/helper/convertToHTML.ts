eb0c55b16618fe4b96479601144672d6f9f61d7d
Avelyn Choi <avelynhc@gmail.com>
2023-09-07 19:39:42 -0400

    Changed folder name and changed method to convert .txt to .html

---
+++ src/helper/convertToHTML.ts
@@ -0,0 +1,26 @@
+const htmlFs = require("fs");
+
+module.exports.convertToHTML = function (title: string, cssLink: any, body: any, outputFolder: any) {
+    const result = `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    ${
+        cssLink
+            ? `<link rel='stylesheet' href='${cssLink}'>`
+            : ""
+    }
+</head>
+<h1>${title}</h1>
+<body>
+    ${body}
+</body>
+</html>`;
+
+    htmlFs.writeFile(`${outputFolder}/${title}.html`, result, function (err: any) {
+        if (err) console.error(err);
+    });
+    console.log(`${title}.html is created successfully!`);
+};
---
+++ src/helper/readFile.ts
@@ -0,0 +1,23 @@
+import readFileFs from "fs";
+import readFilePath from "path";
+const html = require("./convertToHTML");
+let body = "";
+
+module.exports.readFile = function (inputPath: any, cssLink: any, outputFolder: any) {
+    // parse a title from the input file, which will be used to populate <title>...</title>
+    const title = readFilePath.basename(inputPath, ".txt");
+
+    try {
+        const data = readFileFs.readFileSync(inputPath, "utf8");
+        body = data
+            .split(/\r?\n\r?\n/)
+            .map((paragraph: string) =>
+                `<p>${paragraph.replace(/\r?\n/, " ")}</p>\n\n`)
+            .join("");
+    } catch (err) {
+        console.error(err);
+    }
+
+    html.convertToHTML(title, cssLink, body, outputFolder);
+    return title;
+};

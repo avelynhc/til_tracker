da443ede25241e01ae93efb3928da212a35cfb29
Amnish <amnishsingh04@gmail.com>
2023-09-22 21:42:08 -0400

    Rename enum HtmlTokens to MarkdownTokens

--- src/helper/markdown/lineQueries.ts
+++ src/helper/markdown/lineQueries.ts
@@ -1,5 +1,5 @@
 // Special Tokens
-enum HtmlTokens {
+enum MarkdownTokens {
     H1 = '# ',
     H2 = '## ',
     H3 = '### ',
@@ -9,27 +9,27 @@ enum HtmlTokens {
 }
 
 function isHeadingLevel1(line: string): boolean {
-    return line.startsWith(HtmlTokens.H1);
+    return line.startsWith(MarkdownTokens.H1);
 }
 
 function isHeadingLevel2(line: string): boolean {
-    return line.startsWith(HtmlTokens.H2);
+    return line.startsWith(MarkdownTokens.H2);
 }
 
 function isHeadingLevel3(line: string): boolean {
-    return line.startsWith(HtmlTokens.H3);
+    return line.startsWith(MarkdownTokens.H3);
 }
 
 function isHeadingLevel4(line: string): boolean {
-    return line.startsWith(HtmlTokens.H4);
+    return line.startsWith(MarkdownTokens.H4);
 }
 
 function isHeadingLevel5(line: string): boolean {
-    return line.startsWith(HtmlTokens.H5);
+    return line.startsWith(MarkdownTokens.H5);
 }
 
 function isHeadingLevel6(line: string): boolean {
-    return line.startsWith(HtmlTokens.H6);
+    return line.startsWith(MarkdownTokens.H6);
 }
 
 // Line formatter
@@ -37,22 +37,22 @@ export function formatHtmlForMarkdownLine(line: string) {
     let formattedLine = line;
 
     if (isHeadingLevel1(line)) {
-        formattedLine = `<h1>${line.replace(HtmlTokens.H1, '')}</h1>`;
+        formattedLine = `<h1>${line.replace(MarkdownTokens.H1, '')}</h1>`;
     }
     else if (isHeadingLevel2(line)) {
-        formattedLine = `<h2>${line.replace(HtmlTokens.H2, '')}</h2>`;
+        formattedLine = `<h2>${line.replace(MarkdownTokens.H2, '')}</h2>`;
     }
     else if (isHeadingLevel3(line)) {
-        formattedLine = `<h3>${line.replace(HtmlTokens.H3, '')}</h3>`;
+        formattedLine = `<h3>${line.replace(MarkdownTokens.H3, '')}</h3>`;
     }
     else if (isHeadingLevel4(line)) {
-        formattedLine = `<h4>${line.replace(HtmlTokens.H4, '')}</h4>`;
+        formattedLine = `<h4>${line.replace(MarkdownTokens.H4, '')}</h4>`;
     }
     else if (isHeadingLevel5(line)) {
-        formattedLine = `<h5>${line.replace(HtmlTokens.H5, '')}</h5>`;
+        formattedLine = `<h5>${line.replace(MarkdownTokens.H5, '')}</h5>`;
     }
     else if (isHeadingLevel6(line)) {
-        formattedLine = `<h6>${line.replace(HtmlTokens.H6, '')}</h6>`;
+        formattedLine = `<h6>${line.replace(MarkdownTokens.H6, '')}</h6>`;
     }
     else {
         // Default to paragraph

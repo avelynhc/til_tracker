450e8710d2e726eaf6244b3095dd5c4378935b87
Avelyn Choi <avelynhc@gmail.com>
2023-09-24 12:16:32 -0400

    code clean up with regex for conversion from .md to .html

--- src/helper/htmlConversion.ts
+++ src/helper/htmlConversion.ts
@@ -2,7 +2,7 @@ import * as htmlFs from 'fs';
 
 export const SUPPORTED_FILE_EXTENSIONS = ['.md', '.txt'];
 
-export function convertToHTML(title: string, cssLink: string, body: string, selectedLang: string, outputFolder: string) {
+export function htmlConversion(title: string, cssLink: string, body: string, selectedLang: string, outputFolder: string) {
     const result:string = `<!doctype html>
 <html lang="${selectedLang}">
 <head>
@@ -30,66 +30,16 @@ ${body}
     console.log(`${title}.html is created successfully!`);
 }
 
-// Special Tokens
-const MarkdownRegexes = {
-    H1: new RegExp(/^# (.+?)(?=\n|$)/g),
-    H2: new RegExp(/^## (.+?)(?=\n|$)/g),
-    H3: new RegExp(/^### (.+?)(?=\n|$)/g),
-    H4: new RegExp(/^#### (.+?)(?=\n|$)/g),
-    H5: new RegExp(/^##### (.+?)(?=\n|$)/g),
-    H6: new RegExp(/^###### (.+?)(?=\n|$)/g),
-}
-
-function isHeadingLevel1(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H1);
-}
-
-function isHeadingLevel2(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H2);
-}
-
-function isHeadingLevel3(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H3);
-}
-
-function isHeadingLevel4(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H4);
-}
-
-function isHeadingLevel5(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H5);
-}
-
-function isHeadingLevel6(line: string): boolean {
-    return !!line.match(MarkdownRegexes.H6);
-}
-
-// Line formatter
 export function formatHtmlForMarkdownLine(line: string) {
-    let formattedLine = line;
-
-    if (isHeadingLevel1(line)) {
-        formattedLine = `<h1>${line.replace(MarkdownRegexes.H1, '$1')}</h1>`;
-    }
-    else if (isHeadingLevel2(line)) {
-        formattedLine = `<h2>${line.replace(MarkdownRegexes.H2, '$1')}</h2>`;
-    }
-    else if (isHeadingLevel3(line)) {
-        formattedLine = `<h3>${line.replace(MarkdownRegexes.H3, '$1')}</h3>`;
-    }
-    else if (isHeadingLevel4(line)) {
-        formattedLine = `<h4>${line.replace(MarkdownRegexes.H4, '$1')}</h4>`;
-    }
-    else if (isHeadingLevel5(line)) {
-        formattedLine = `<h5>${line.replace(MarkdownRegexes.H5, '$1')}</h5>`;
-    }
-    else if (isHeadingLevel6(line)) {
-        formattedLine = `<h6>${line.replace(MarkdownRegexes.H6, '$1')}</h6>`;
-    }
-    else {
-        // Default to paragraph
-        formattedLine = `<p>${line}</p>`
-    }
-
-    return formattedLine;
+    return line
+        .replace(/^#\s(.+)$/gm, '<h1>$1</h1>') // heading 1
+        .replace(/^##\s(.+)$/gm, '<h2>$1</h2>') // heading 2
+        .replace(/^###\s(.+)$/gm, '<h3>$1</h3>') // heading 3
+        .replace(/^####\s(.+)$/gm, '<h4>$1</h4>') // heading 4
+        .replace(/^#####\s(.+)$/gm, '<h5>$1</h5>') // heading 5
+        .replace(/^######\s(.+)$/gm, '<h6>$1</h6>') // heading 6
+        .replace(/^(?!<h[1-6]>|<ul>|<ol>|<li>|<a>).+$/gm, '<p>$&</p>') // paragraph
+        .replace(/\[(.+?)\]\((.+?)\)/g, '<a href=$2>$1</a>') //  link
+        .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') // bold
+        .replace(/\*(.*?)\*/g, '<i>$1</i>') // italic
 }

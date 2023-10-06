ba6c65685483aafd135bf0c34d18515cd55fa5d5
Avelyn Choi <avelynhc@gmail.com>
2023-10-06 13:45:13 -0400

    added check method if it's url. this method was implemented by Amnish04/issue-10

--- src/helper/check.ts
+++ src/helper/check.ts
@@ -1,5 +1,10 @@
 import * as readFilePath from 'path';
 
+export function isURL(str: string): boolean {
+    const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    return urlRegex.test(str);
+}
+
 export function isTextFile(filename: string): boolean {
     return readFilePath.extname(filename) === '.txt';
 }

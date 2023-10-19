7fd2400c4451d8f3728a0f753415dfc98f251778
Avelyn Choi <avelynhc@gmail.com>
2023-10-19 17:21:51 -0400

    code clean up

--- src/helper/fileHandler.ts
+++ src/helper/fileHandler.ts
@@ -1,6 +1,6 @@
 import * as readFileFs from 'fs';
 import * as readFilePath from 'path';
-import { htmlConversion, formatHtmlForMarkdownLine } from './htmlConversion';
+import { htmlConversion } from './htmlConversion';
 import { errorHandling } from '../fileParser';
 import { isMarkdownFile, isTextFile } from "./check";
 let body: string = '';
@@ -49,4 +49,18 @@ function parseMarkdownFileContent(content: string): string {
             return formatHtmlForMarkdownLine(line)
         })
         .join('\n'); // Join all the generated tags
+}
+
+function formatHtmlForMarkdownLine(line: string) {
+    return line
+        .replace(/^#\s(.+)$/gm, '<h1>$1</h1>') // heading 1
+        .replace(/^##\s(.+)$/gm, '<h2>$1</h2>') // heading 2
+        .replace(/^###\s(.+)$/gm, '<h3>$1</h3>') // heading 3
+        .replace(/^####\s(.+)$/gm, '<h4>$1</h4>') // heading 4
+        .replace(/^#####\s(.+)$/gm, '<h5>$1</h5>') // heading 5
+        .replace(/^######\s(.+)$/gm, '<h6>$1</h6>') // heading 6
+        .replace(/^(?!<h[1-6]>|<ul>|<ol>|<li>|<a>).+$/gm, '<p>$&</p>') // paragraph
+        .replace(/\[(.+?)\]\((.+?)\)/g, '<a href=$2>$1</a>') //  link
+        .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') // bold
+        .replace(/\*(.*?)\*/g, '<i>$1</i>') // italic
 }
--- src/helper/htmlConversion.ts
+++ src/helper/htmlConversion.ts
@@ -28,17 +28,3 @@ ${body}
     });
     console.log(`${title}.html is created successfully!`);
 }
-
-export function formatHtmlForMarkdownLine(line: string) {
-    return line
-        .replace(/^#\s(.+)$/gm, '<h1>$1</h1>') // heading 1
-        .replace(/^##\s(.+)$/gm, '<h2>$1</h2>') // heading 2
-        .replace(/^###\s(.+)$/gm, '<h3>$1</h3>') // heading 3
-        .replace(/^####\s(.+)$/gm, '<h4>$1</h4>') // heading 4
-        .replace(/^#####\s(.+)$/gm, '<h5>$1</h5>') // heading 5
-        .replace(/^######\s(.+)$/gm, '<h6>$1</h6>') // heading 6
-        .replace(/^(?!<h[1-6]>|<ul>|<ol>|<li>|<a>).+$/gm, '<p>$&</p>') // paragraph
-        .replace(/\[(.+?)\]\((.+?)\)/g, '<a href=$2>$1</a>') //  link
-        .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') // bold
-        .replace(/\*(.*?)\*/g, '<i>$1</i>') // italic
-}
